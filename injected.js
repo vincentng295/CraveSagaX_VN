5aec0c9e11ad66820c0c48fb3711daf221a31bde
HuskyDG <151985750+vincentng295@users.noreply.github.com>
2026-08-10 13:28:38 +0700

    Dont save the live translation

--- injected.js
+++ injected.js
@@ -201,21 +201,23 @@ window.addEventListener('message', (event) => {
 
                 translationCache.set(cleanText, translated);
 
+                /*
                 window.postMessage({
                     type: 'SAVE_NEW_TRANSLATION',
                     original: cleanText,
                     translated: translated,
                     seq
-                }, '*');
+                }, '*');*/
 
                 return translated;
             } catch (e) {
+                /*
                 window.postMessage({
                     type: 'SAVE_NEW_TRANSLATION',
                     original: cleanText,
                     translated: '',
                     seq
-                }, '*');
+                }, '*');*/
                 return null;
             }
         }
